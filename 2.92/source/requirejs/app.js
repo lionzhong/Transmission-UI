117181b45f9c10b7ee6f996a6c72603f0a3f40cd
lionzhong <lionzhong@gmail.com>
2017-02-17 14:53:50 +0800

    update

--- 2.92/source/requirejs/app.js
+++ 2.92/source/requirejs/app.js
@@ -4,12 +4,12 @@
 require.config({
     baseUrl: "./javascript/",
     paths: {
-        "jquery": "lib/jquery/jquery.min",
-        "jquery-migrate":"lib/jquery/jquery-migrate.min",
-        "jquery-ui":"lib/jquery/jquery-ui.min",
-        "jquery-ui-contextmenu":"lib/jquery/jquery.ui-contextmenu.min",
-        "jquery-transmenu":"lib/jquery/jquery.transmenu.min",
-        "json2":"lib/json2.min"
+        "jquery": "../javascript/lib/jquery/jquery.min",
+        "jquery-migrate":"../javascript/lib/jquery/jquery-migrate.min",
+        "jquery-ui":"../javascript/lib/jquery/jquery-ui.min",
+        "jquery-ui-contextmenu":"../javascript/lib/jquery/jquery.ui-contextmenu.min",
+        "jquery-transmenu":"../javascript/lib/jquery/jquery.transmenu.min",
+        "json2":"../javascript/lib/json2.min"
     },
     shim: {
         "jquery":{
